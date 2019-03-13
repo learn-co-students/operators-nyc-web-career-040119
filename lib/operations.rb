7fb7f9819a57e227bfaff1a0db4c503806c408b8
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-13 18:32:35 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-
+ if speed < 40 || 60 < speed
+   return TRUE
+ else
+   return FALSE
+ end
 end
 
 
 
 def not_safe?(speed)
-	
+	(speed < 40 || 60 < speed) ? TRUE : FALSE
 end
 	
 
